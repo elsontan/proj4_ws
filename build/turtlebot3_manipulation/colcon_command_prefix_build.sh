c1b4aec292d4a61c12b3b78103ddb4fc591cf50a
Elson Tan Jun Hao <2102036@sit.singaporetech.edu.sg>
2023-01-26 12:49:24 +0800

    Reupdate moveit_config and temporaily removed navigation2 and teleop packages

--- build/turtlebot3_manipulation/colcon_command_prefix_build.sh
+++ build/turtlebot3_manipulation/colcon_command_prefix_build.sh
@@ -2,7 +2,5 @@
 . "/home/elson/proj4_ws/install/turtlebot3_manipulation_cartographer/share/turtlebot3_manipulation_cartographer/package.sh"
 . "/home/elson/proj4_ws/install/turtlebot3_manipulation_description/share/turtlebot3_manipulation_description/package.sh"
 . "/home/elson/proj4_ws/install/turtlebot3_manipulation_hardware/share/turtlebot3_manipulation_hardware/package.sh"
-. "/home/elson/proj4_ws/install/turtlebot3_manipulation_navigation2/share/turtlebot3_manipulation_navigation2/package.sh"
 . "/home/elson/proj4_ws/install/turtlebot3_manipulation_bringup/share/turtlebot3_manipulation_bringup/package.sh"
 . "/home/elson/proj4_ws/install/turtlebot3_manipulation_moveit_config/share/turtlebot3_manipulation_moveit_config/package.sh"
-. "/home/elson/proj4_ws/install/turtlebot3_manipulation_teleop/share/turtlebot3_manipulation_teleop/package.sh"
